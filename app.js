c913e0598c863913900db82d26b2889dc126c81e
johnny_wang <johnny1001@top-link.com.tw>
2019-03-19 17:55:17 +0800

    j

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+// superagent 請求套件: 爬取 DOM
+// cheerio 類 JQuery 套件: 模擬 JQuery 控制抓取的 DOM
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+
+app.get('/', function(req, res, next){
+  // console.log(req);
+  superagent.get('https://rate.bot.com.tw/xrt?Lang=zh-TW')
+    .end((err, data)=>{
+      if (err) throw err;
+      // data.text 為該網頁 html 內容，將它傳給 cheerio.load 之後
+      // 就可以得到一个實現了 jquery 接口的變量，我们習慣性地將它命名為 `$`
+      // 剩下就都是 jquery 的内容了
+      let $ = cheerio.load(data.text);
+      let items = [];
+      $('table[title="牌告匯率"] > tbody').children('tr').each((i, element)=>{
+        let $element = $(element);
+        items.push({
+          title: $element.children('td[data-table="幣別"]').find('.hidden-phone').text().trim(),
+          cash: {
+            buy: $element.children('td[data-table="本行現金買入"]').text(),
+            sale: $element.children('td[data-table="本行現金賣出"]').text()
+          },
+          recent: {
+            buy: $element.children('td[data-table="本行即期買入"]').text(),
+            sale: $element.children('td[data-table="本行即期賣出"]').text()
+          }
+        });
+      });
+
+      res.send(items);
+    });
+});
+
+app.listen(3000, function(){
+  console.log('Express Server listening on port: 3000');
+});

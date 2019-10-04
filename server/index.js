645aece0ec07d212986362842ef06b1535e11828
johnnywang <johnnywang@Johnny-MacBookPro.local>
2019-10-04 14:29:50 +0800

    update port listen

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ const viewRouter = require('./view-router')
 const apiRouter = require('./api-router')
 
 
+app.set('port', process.env.PORT || 8080);
+
 app.use('/', viewRouter)
 
 app.use('/api', apiRouter)
 
 
-app.listen(8080)
+app.listen(app.get('port'), function(){
+  console.log('Express Server listening on port: '+app.get('port'));
+});
